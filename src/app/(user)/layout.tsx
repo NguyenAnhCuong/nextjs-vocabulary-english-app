93c17a7f89430358d1cbb8e8697bdf7beae848b3
cuong <cuong1606x@gmail.com>
2026-04-06 19:07:50 +0700

    dynamic

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -3,6 +3,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/auth.options";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function UserLayout({
   children,
 }: {
